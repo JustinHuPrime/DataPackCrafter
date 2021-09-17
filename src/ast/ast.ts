c8ae7f3ce259a503dbeb4f228d6c606384bdf38e
Justin Hu <justin.hu@alumni.ubc.ca>
2021-09-16 19:18:27 -0700

    add span utility class

--- src/ast/ast.ts
+++ src/ast/ast.ts
@@ -1,1 +1,34 @@
-export default class Ast {}
+export default class Ast {
+  span: Span;
+
+  constructor(span: Span) {
+    this.span = span;
+  }
+}
+
+export class Span {
+  start: {
+    line: number;
+    character: number;
+  };
+  end: {
+    line: number;
+    character: number;
+  };
+
+  constructor(
+    startLine: number,
+    startCharacter: number,
+    endLine: number,
+    endCharacter: number,
+  ) {
+    this.start = {
+      line: startLine,
+      character: startCharacter,
+    };
+    this.end = {
+      line: endLine,
+      character: endCharacter,
+    };
+  }
+}
