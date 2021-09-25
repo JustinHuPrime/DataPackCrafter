e000dc94649c19ed266b20dcee50ea47d4add8d0
James Lu <james@overdrivenetworks.com>
2021-09-25 13:26:24 -0700

    Extend AstVisitor arguments with EvaluatorEnv

--- src/ast/visitor.ts
+++ src/ast/visitor.ts
@@ -1,24 +1,25 @@
+import { EvaluatorEnv } from "../codeGenerator/evaluator";
 import { Import, Define, Let, If, For, Print, Binop, Unop, Index, Slice, Call, List, Begin, On, Advancement, True, False } from "./ast";
 
 export interface AstVisitor {
-  visitImport(astNode: Import) : void;
-  visitDefine(astNode: Define) : void;
-  visitLet(astNode: Let) : void;
-  visitIf(astNode: If) : void;
-  visitFor(astNode: For) : void;
-  visitPrint(astNode: Print) : void;
-  visitBinop(astNode: Binop) : void;
-  visitUnop(astNode: Unop) : void;
-  visitIndex(astNode: Index) : void;
-  visitSlice(astNode: Slice) : void;
-  visitCall(astNode: Call) : void;
-  visitList(astNode: List) : void;
-  visitBegin(astNode: Begin) : void;
-  visitOn(astNode: On) : void;
-  visitAdvancement(astNode: Advancement) : void;
-  visitFunction(astNode: Function) : void;
-  visitTrue(astNode: True) : void;
-  visitFalse(astNode: False) : void;
-  visitNumber(astNode: Number) : void;
-  visitString(astNode: String) : void;
+  visitImport(astNode: Import, env: EvaluatorEnv) : any;
+  visitDefine(astNode: Define, env: EvaluatorEnv) : any;
+  visitLet(astNode: Let, env: EvaluatorEnv) : any;
+  visitIf(astNode: If, env: EvaluatorEnv) : any;
+  visitFor(astNode: For, env: EvaluatorEnv) : any;
+  visitPrint(astNode: Print, env: EvaluatorEnv) : any;
+  visitBinop(astNode: Binop, env: EvaluatorEnv) : any;
+  visitUnop(astNode: Unop, env: EvaluatorEnv) : any;
+  visitIndex(astNode: Index, env: EvaluatorEnv) : any;
+  visitSlice(astNode: Slice, env: EvaluatorEnv) : any;
+  visitCall(astNode: Call, env: EvaluatorEnv) : any;
+  visitList(astNode: List, env: EvaluatorEnv) : any;
+  visitBegin(astNode: Begin, env: EvaluatorEnv) : any;
+  visitOn(astNode: On, env: EvaluatorEnv) : any;
+  visitAdvancement(astNode: Advancement, env: EvaluatorEnv) : any;
+  visitFunction(astNode: Function, env: EvaluatorEnv) : any;
+  visitTrue(astNode: True, env: EvaluatorEnv) : any;
+  visitFalse(astNode: False, env: EvaluatorEnv) : any;
+  visitNumber(astNode: Number, env: EvaluatorEnv) : any;
+  visitString(astNode: String, env: EvaluatorEnv) : any;
 }
--- src/codeGenerator/evaluator.ts
+++ src/codeGenerator/evaluator.ts
@@ -70,64 +70,64 @@ export class FunctionClosure {
 
 
 export class Evaluator implements AstVisitor {
-    visitImport(_astNode: Import) : void{
+    visitImport(_astNode: Import, _env: EvaluatorEnv) : EvaluatorData {
         throw new Error("Method not implemented.");
     }
-    visitDefine(_astNode: Define) : void{
+    visitDefine(_astNode: Define, _env: EvaluatorEnv) : EvaluatorData {
         throw new Error("Method not implemented.");
     }
-    visitLet(_astNode: Let) : void{
+    visitLet(_astNode: Let, _env: EvaluatorEnv) : EvaluatorData {
         throw new Error("Method not implemented.");
     }
-    visitIf(_astNode: If) : void{
+    visitIf(_astNode: If, _env: EvaluatorEnv) : EvaluatorData {
         throw new Error("Method not implemented.");
     }
-    visitFor(_astNode: For) : void{
+    visitFor(_astNode: For, _env: EvaluatorEnv) : EvaluatorData {
         throw new Error("Method not implemented.");
     }
-    visitPrint(_astNode: Print) : void{
+    visitPrint(_astNode: Print, _env: EvaluatorEnv) : EvaluatorData {
         throw new Error("Method not implemented.");
     }
-    visitBinop(_astNode: Binop) : void{
+    visitBinop(_astNode: Binop, _env: EvaluatorEnv) : EvaluatorData {
         throw new Error("Method not implemented.");
     }
-    visitUnop(_astNode: Unop) : void{
+    visitUnop(_astNode: Unop, _env: EvaluatorEnv) : EvaluatorData {
         throw new Error("Method not implemented.");
     }
-    visitIndex(_astNode: Index) : void{
+    visitIndex(_astNode: Index, _env: EvaluatorEnv) : EvaluatorData {
         throw new Error("Method not implemented.");
     }
-    visitSlice(_astNode: Slice) : void{
+    visitSlice(_astNode: Slice, _env: EvaluatorEnv) : EvaluatorData {
         throw new Error("Method not implemented.");
     }
-    visitCall(_astNode: Call) : void{
+    visitCall(_astNode: Call, _env: EvaluatorEnv) : EvaluatorData {
         throw new Error("Method not implemented.");
     }
-    visitList(_astNode: List) : void{
+    visitList(_astNode: List, _env: EvaluatorEnv) : EvaluatorData {
         throw new Error("Method not implemented.");
     }
-    visitBegin(_astNode: Begin) : void{
+    visitBegin(_astNode: Begin, _env: EvaluatorEnv) : EvaluatorData {
         throw new Error("Method not implemented.");
     }
-    visitOn(_astNode: On) : void{
+    visitOn(_astNode: On, _env: EvaluatorEnv) : EvaluatorData {
         throw new Error("Method not implemented.");
     }
-    visitAdvancement(_astNode: Advancement) : void{
+    visitAdvancement(_astNode: Advancement, _env: EvaluatorEnv) : EvaluatorData {
         throw new Error("Method not implemented.");
     }
-    visitFunction(_astNode: Function) : void{
+    visitFunction(_astNode: Function, _env: EvaluatorEnv) : EvaluatorData {
         throw new Error("Method not implemented.");
     }
-    visitTrue(_astNode: True) : void{
+    visitTrue(_astNode: True, _env: EvaluatorEnv) : EvaluatorData {
         throw new Error("Method not implemented.");
     }
-    visitFalse(_astNode: False) : void{
+    visitFalse(_astNode: False, _env: EvaluatorEnv) : EvaluatorData {
         throw new Error("Method not implemented.");
     }
-    visitNumber(_astNode: Number) : void{
+    visitNumber(_astNode: Number, _env: EvaluatorEnv) : EvaluatorData {
         throw new Error("Method not implemented.");
     }
-    visitString(_astNode: String) : void{
+    visitString(_astNode: String, _env: EvaluatorEnv) : EvaluatorData {
         throw new Error("Method not implemented.");
     }
 
