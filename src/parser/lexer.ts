9bba19d76b0b1c393678dd9ae6dba2ca29e3cd5b
Wong Kean Wah <2560596+kilowatt-@users.noreply.github.com>
2021-09-27 17:54:54 -0700

    added helper function in lexer

--- src/parser/lexer.ts
+++ src/parser/lexer.ts
@@ -7,12 +7,44 @@ export default class Lexer {
   line: number;
   character: number;
   filename: string;
+  literals: Set<string>;
 
   constructor(filename: string) {
     this.filename = filename;
     this.file = fs.readFileSync(filename, "utf8");
     this.line = 1;
     this.character = 1;
+
+    const literalArr = ['datapack',
+    'import',
+    'define',
+    'let',
+    'if',
+    'then',
+    'else',
+    'for',
+    'in',
+    'print',
+    'on',
+    'advancement',
+    'function',
+    'true',
+    'false',
+    'grant',
+    'revoke',
+      'load',
+      'tick',
+      'consume_item',
+      'inventory_changed',
+      'item',
+      'tag',
+      'title',
+      'icon',
+      'description',
+      'parent'
+    ]
+
+    this.literals = new Set(literalArr);
   }
 
   removeWhiteSpace(): void {
@@ -37,8 +69,19 @@ export default class Lexer {
     }
   }
 
-  lexRegular(): Token {
+  private lexToken(matcher: RegExpMatchArray, type: TokenType): Token {
+    const tokenString = matcher[0] || ''; // TS likes to break things
 
+    const start: Location = new Location(this.line, this.character);
+    const end : Location = new Location(this.line, this.character + tokenString.length);
+
+    this.character += tokenString.length;
+    this.file = this.file.substr(tokenString.length);
+
+    return new Token(type, new Span(start, end), tokenString);
+  }
+
+  lexRegular(): Token {
     this.removeWhiteSpace();
 
     if (this.file === "") {
@@ -48,17 +91,14 @@ export default class Lexer {
     }
 
     const matchIdToken = this.file.match(/^[a-zA-Z_][a-zA-Z_0-9]*/);
+    const matchNumberToken = this.file.match(/^'-'? [1-9][0-9]+ ('.' [0-9]+)?/);
 
-    if (matchIdToken) {
+    if (matchIdToken && matchIdToken[0] && matchIdToken.index === 0) {
       const tokenString = matchIdToken[0];
-
-      const start: Location = new Location(this.line, this.character);
-      const end : Location = new Location(this.line, this.character + tokenString.length);
-
-      this.character += tokenString.length;
-      this.file = this.file.substr(tokenString.length);
-
-      return new Token(TokenType.ID, new Span(start, end), tokenString);
+      const tokenType = (this.literals.has(tokenString)) ? TokenType.LITERAL : TokenType.ID;
+      return this.lexToken(matchIdToken, tokenType)
+    } else if (matchNumberToken && matchNumberToken[0] && matchNumberToken.index === 0) {
+      return this.lexToken(matchNumberToken, TokenType.NUMBER);
     }
 
     throw new LexerError(`${this.filename}: ${this.line}:${this.character}: error: invalid character '${this.file[0]}`);
@@ -69,6 +109,4 @@ export default class Lexer {
   }
 }
 
-export class LexerError extends Error {
-
-}
+export class LexerError extends Error {}
---
+++ test/parser/lexer.spec.ts
@@ -0,0 +1,5 @@
+import Lexer from "../../src/parser/lexer";
+
+describe("lexer", () => {
+  console.log(Lexer);
+});
