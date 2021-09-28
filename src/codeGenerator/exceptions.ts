6fefb2166d25d645783650b486659456fdae4b3f
James Lu <james@overdrivenetworks.com>
2021-09-27 22:11:54 -0700

    evaluator: add custom exception classes

---
+++ src/codeGenerator/exceptions.ts
@@ -0,0 +1,60 @@
+import { Expression } from "../ast/ast";
+
+/**
+ * Generic class for evaluator errors.
+ * @expr    the expression that caused the error
+ * @message a detailed description of what went wrong
+ */
+export class DSLEvaluationError extends Error {
+    expr: Expression | null;
+
+    constructor(expr: Expression, message: string) {
+        super(message);
+        this.name = "DSLEvaluationError"
+        this.expr = expr;
+    }
+}
+
+/**
+ * Error thrown when using an unknown (unbound) variable
+ */
+export class DSLReferenceError extends DSLEvaluationError {
+    constructor(expr: Expression, message: string) {
+        super(expr, message);
+        this.name = "DSLReferenceError"
+        this.expr = expr;
+    }
+}
+
+/**
+ * Math error (overflow or divide by zero)
+ */
+export class DSLMathError extends DSLEvaluationError {
+    constructor(expr: Expression, message: string) {
+        super(expr, message);
+        this.name = "DSLMathError"
+        this.expr = expr;
+    }
+}
+
+/**
+ * Malformed expressions (e.g. let with mismatched argument lists)
+ */
+export class DSLSyntaxError extends DSLEvaluationError {
+    constructor(expr: Expression, message: string) {
+        super(expr, message);
+        this.name = "DSLSyntaxError"
+        this.expr = expr;
+    }
+}
+
+/**
+ * Type errors
+ */
+export class DSLTypeError extends DSLEvaluationError {
+    constructor(expr: Expression, message: string) {
+        super(expr, message);
+        this.name = "DSLTypeError"
+        this.expr = expr;
+    }
+}
